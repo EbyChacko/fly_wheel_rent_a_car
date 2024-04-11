8313e01196125fe4a63a72b86b0b19f50083c61e
EbyChacko <cybe1990@gmail.com>
2024-04-11 17:35:41 +0100

    add about,contact pages

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('About_Us/', views.about, name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 def index(request):
     """To return home page"""
     return render(request, 'home/index.html')
+
+def contact(request):
+    """To return contact page"""
+    return render(request, 'home/contact.html')
+
+def about(request):
+    """To return contact page"""
+    return render(request, 'home/about.html')
