90003021e64ae90514b63db7d32f39f15453daee
Eby Chacko <cybe1990@gmail.com>
2024-05-28 10:14:43 +0000

    feat: add delete option for the booking

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('booking_details/<int:id>/', views.booking_details, name='booking_details'),
-
+    path('delete_booking/<int:id>/', views.delete_booking, name='delete_booking'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from cars.models import PersonalDetails
 from checkout.models import Booking
@@ -12,15 +12,17 @@ def profile(request):
         customer_details = None
 
     try:
-        bookings = Booking.objects.filter(customer=customer_details) if customer_details else []
+        bookings = Booking.objects.filter(customer=customer_details).order_by('-booking_time') if customer_details else []
         if not bookings:
             messages.info(request, 'Sorry, no bookings yet for this profile.')
     except Exception as e:
         messages.error(request, f'Sorry, an error occurred: {e}')
 
+    now = datetime.now()
     context = {
         'customer_details': customer_details,
-        'bookings': bookings
+        'bookings': bookings,
+        'now': now,
     }
     return render(request, 'profiles/profile.html', context)
 
@@ -36,7 +38,6 @@ def booking_details(request, id):
     pick_up_datetime = datetime.combine(booking.pick_up_date, booking.pick_up_time)
     time_difference = pick_up_datetime - now
     disable_delete = time_difference < timedelta(hours=48)
-    disable_cancel = pick_up_datetime < now
     context = {
         'booking': booking,
         'disable_delete': disable_delete,
@@ -45,3 +46,23 @@ def booking_details(request, id):
         'pick_up_datetime':pick_up_datetime,
     }
     return render(request, 'profiles/booking_details.html', context)
+
+
+def delete_booking(request, id):
+    booking = get_object_or_404(Booking, id=id)
+    
+    if request.user != booking.customer.user:
+        messages.error(request, "You are not authorized to delete this booking.")
+        return redirect('profile')
+
+    now = datetime.now()
+    pick_up_datetime = datetime.combine(booking.pick_up_date, booking.pick_up_time)
+    time_difference = pick_up_datetime - now
+    disable_delete = time_difference < timedelta(hours=48)
+    if disable_delete:
+        messages.error(request, "Cannot delete the booking within 48 hours of the pickup time.")
+        return redirect('profile')
+
+    booking.delete()
+    messages.success(request, "Booking deleted successfully.")
+    return redirect('profile')
