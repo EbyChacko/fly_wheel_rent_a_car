f8133a8c72c0030465229b5202e8c0bd0f77f817
Eby Chacko <cybe1990@gmail.com>
2024-06-03 20:19:27 +0000

    fix:issue with the email not showing any data

--- checkout/signals.py
+++ checkout/signals.py
@@ -7,13 +7,26 @@ from .models import Booking
 @receiver(post_save, sender=Booking)
 def send_booking_confirmation(sender, instance, created, **kwargs):
     if created:
-        # Booking was just created
         subject = 'Booking Confirmation - FlyWheel Rent a Car'
-        from_email = 'noreply@flywheelrentacar.com'
+        from_email = 'flywheelrent@gmail.com'
         to_email = instance.email
         
         context = {
             'booking': instance,
+            'name':instance.name,
+            'booking_number':instance.booking_number,
+            'make':instance.car.make,
+            'model':instance.car.model,
+            'category':instance.car.category,
+            'pick_up_city':instance.pick_up_city,
+            'pick_up_county':instance.pick_up_county,
+            'pick_up_date':instance.pick_up_date,
+            'pick_up_time':instance.pick_up_time,
+            'drop_off_city':instance.drop_off_city,
+            'drop_off_county':instance.drop_off_county,
+            'drop_off_date':instance.drop_off_date,
+            'drop_off_time':instance.drop_off_time,
+            'grand_total':instance.grand_total,
         }
         
         text_content = render_to_string('emails/booking_confirmation.txt', context)
